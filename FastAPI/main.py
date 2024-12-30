7d33c9dd297ca02a99eecd9559fdff4da440a040
sultantemuruly <sultantemuruly@gmail.com>
2024-12-30 16:30:22 +0500

    main commit

---
+++ FastAPI/main.py
@@ -0,0 +1,80 @@
+from fastapi import FastAPI, HTTPException, Depends
+from typing import Annotated, List
+from sqlalchemy.orm import Session
+from pydantic import BaseModel
+from database import SessionLocal, engine
+import models
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = ["http://localhost:3000"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+class TransactionBase(BaseModel):
+    amount: float
+    category: str
+    description: str
+    is_income: bool
+    date: str
+
+
+class TransactionModel(TransactionBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
+def get_db():
+    db = SessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+models.Base.metadata.create_all(bind=engine)
+
+
+@app.get("/")
+async def root():
+    return {"Message:", "Hello World!"}
+
+
+@app.post("/transactions/", response_model=TransactionModel)
+async def create_transaction(transaction: TransactionBase, db: db_dependency):
+    db_transaction = models.Transaction(**transaction.dict())
+    db.add(db_transaction)
+    db.commit()
+    db.refresh(db_transaction)
+    return db_transaction
+
+
+@app.get("/transactions", response_model=List[TransactionModel])
+async def read_transactions(db: db_dependency, limit: int = 100):
+    transactions = db.query(models.Transaction).limit(limit).all()
+    return transactions
+
+
+@app.delete("/clear-db")
+async def clear_db(db: db_dependency):
+    try:
+        models.Base.metadata.drop_all(bind=engine)
+        models.Base.metadata.create_all(bind=engine)
+        return {"message": "Database cleared and tables recreated successfully."}
+    except Exception as e:
+        raise HTTPException(
+            status_code=500, detail=f"Failed to clear the database: {str(e)}"
+        )
